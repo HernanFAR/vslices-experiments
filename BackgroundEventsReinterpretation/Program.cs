2193f7d87956ffddbdc26352ab154d181cedb66e
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2024-07-04 23:47:53 -0400

    Agregar archivos de proyecto.

---
+++ BackgroundEventsReinterpretation/Program.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Hello, World!");
+
+await UseMsExtHosting();
+
+return; 
+
+async ValueTask UseMsExtHosting()
+{
+    IHost host = new HostBuilder()
+        .ConfigureServices((hostContext, services) =>
+        {
+            services
+                .AddSingleton<IBackgroundTaskListener, HostedBackgroundTaskListener>()
+                .AddHostedService<HostedBackgroundTaskListener>()
+                .AddSingleton<EventCountConfiguration>()
+                .AddSingleton<IBackgroundTask, EvenCount>()
+                .AddSingleton<IBackgroundTask, OddCount>();
+        })
+        .Build();
+
+    await host.RunAsync();
+}
+
+public interface IBackgroundTaskListener
+{
+    ValueTask ExecuteRegisteredJobs(CancellationToken cancellationToken);
+
+}
+
+public sealed class HostedBackgroundTaskListener(IEnumerable<IBackgroundTask> backgroundTasks) 
+    : BackgroundService, IBackgroundTaskListener
+{
+    readonly IEnumerable<IBackgroundTask> _backgroundTasks = backgroundTasks;
+
+    public async ValueTask ExecuteRegisteredJobs(CancellationToken cancellationToken)
+    {
+        await Task.WhenAll(_backgroundTasks.Select(task => task.ExecuteAsync(cancellationToken).AsTask()));
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await ExecuteRegisteredJobs(stoppingToken);
+    }
+}
+
+public interface IBackgroundTask
+{
+    ValueTask ExecuteAsync(CancellationToken cancellationToken);
+}
+
+public sealed class EventCountConfiguration
+{
+    public int MaxNumber => 1000;
+}
+
+public sealed class EvenCount(EventCountConfiguration config) : IBackgroundTask
+{
+    public async ValueTask ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var currentCount = 0;
+
+        while (true)
+        {
+            await Task.Delay(1000, cancellationToken);
+
+            if (currentCount++ >= config.MaxNumber)
+            {
+                break;
+            }
+
+            if (currentCount % 2 == 0)
+            {
+                Console.WriteLine($"Contando número par: {currentCount}");    
+            }
+        }
+    }
+}
+
+public sealed class OddCount(EventCountConfiguration config) : IBackgroundTask
+{
+    public async ValueTask ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var currentCount = 0;
+
+        while (true)
+        {
+            await Task.Delay(1000, cancellationToken);
+
+            if (currentCount++ >= config.MaxNumber)
+            {
+                break;
+            }
+
+            if (currentCount % 2 == 1)
+            {
+                Console.WriteLine($"Contando número impar: {currentCount}");
+            }   
+        }
+    }
+}
---
+++ LanguageExtIntegration/Program.cs
@@ -0,0 +1,45 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+using static LanguageExt.Prelude;
+
+namespace VSlicesLanguageExtIntegration;
+
+interface IFeature<TResult> { }
+record Feature(string Name) : IFeature<Unit>;
+
+interface IFailure
+{
+    string Title { get; }
+
+    string? Detail { get; }
+
+    Dictionary<string, object?> Extensions { get; }
+
+}
+
+public sealed record DefaultFailure(
+    string Title,
+    string? Detail,
+    Dictionary<string, object?> Extensions) : IFailure
+{
+    public DefaultFailure(string title, string? detail)
+        : this(title, detail, new Dictionary<string, object?>()) { }
+}
+
+interface IHandler<in TRequest, TResult> : IFeature<TResult> 
+    where TRequest : IFeature<TResult>
+{
+    Aff<TResult> HandlerAsync(TRequest request, CancellationToken cancellationToken);
+
+}
+
+class Handler : IHandler<Feature, Unit>
+{
+    public Aff<Unit> HandlerAsync(Feature request, CancellationToken cancellationToken)
+    {
+        return Aff(async () =>
+        {
+            return unit;
+        });
+    }
+}
